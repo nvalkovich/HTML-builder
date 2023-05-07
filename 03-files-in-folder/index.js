cd0bce5aaba2c349f40ea23b3544f729d81471ee
nvalkovich <anastasiya.valkovich@mail.ru>
2023-05-07 03:54:39 +0300

    feat: implement 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const pathToDir = path.join(__dirname, 'secret-folder');
+
+async function readFilesInDir() {
+  const files = await fs.readdir(pathToDir, { withFileTypes: true });
+
+  files.forEach(async (file) => {
+    if (file.isDirectory()) return;
+
+    const pathToFile = path.join(pathToDir, file.name);
+    const stat = await fs.stat(pathToFile);
+    const ext = path.extname(file.name).slice(1);;
+    const name = path.parse(file.name).name;
+    const size = stat.size;
+    
+    console.log(`${name} - ${ext} - ${size}kb`);
+  })
+}
+
+readFilesInDir();
